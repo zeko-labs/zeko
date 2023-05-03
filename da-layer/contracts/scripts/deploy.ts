8ff22b65cc4d9729a8bb7794802ec7e7c6417f00
Martin Ondejka <martin.ondejka@vacuumlabs.com>
2023-05-03 23:31:35 +0200

    Add initial sequencer

---
+++ da-layer/contracts/scripts/deploy.ts
@@ -0,0 +1,41 @@
+import { ethers } from "hardhat";
+import { PrivateKey, isReady as isSnarkyjsReady, shutdown as shutdownSnarkyjs } from "snarkyjs";
+import { createCombinedArtifact } from "../utils/abi";
+import { fieldToHex } from "../utils/mina";
+
+const main = async () => {
+  await isSnarkyjsReady;
+
+  const upgradeableArtifact = await createCombinedArtifact(
+    "DataAvailabilityProxy",
+    "DataAvailability"
+  );
+
+  const implementationFactory = await ethers.getContractFactory("DataAvailability");
+  const proxyFactory = await ethers.getContractFactoryFromArtifact(upgradeableArtifact);
+
+  const implementation = await implementationFactory.deploy();
+
+  await implementation.deployed();
+
+  const validators = Array.from({ length: 5 }, () => PrivateKey.random());
+
+  // TODO: set quorum and validators
+  const proxy = await proxyFactory.deploy(
+    implementation.address,
+    validators.length,
+    validators.map((validator) => {
+      const { x, y } = validator.toPublicKey().toGroup();
+      return {
+        x: fieldToHex(x),
+        y: fieldToHex(y),
+      };
+    })
+  );
+
+  console.log("DataAvailabilityProxy deployed to:", proxy.address);
+
+  await shutdownSnarkyjs();
+};
+
+main().catch(console.error);
