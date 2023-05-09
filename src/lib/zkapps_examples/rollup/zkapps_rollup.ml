88fc98121ec51a6beb6e7e02c67f8ac359eb6dca
Las Safin <me@las.rs>
2023-05-09 07:21:43 +0000

    Compile zkApp

--- src/lib/zkapps_examples/rollup/zkapps_rollup.ml
+++ src/lib/zkapps_examples/rollup/zkapps_rollup.ml
@@ -7,82 +7,154 @@ open Snark_params
 open Tick
 open Run
 
-let initialize public_key =
-  Zkapps_examples.wrap_main
-    ~public_key:(Public_key.Compressed.var_of_t public_key)
-    (fun account_update -> account_update#assert_state_unproved )
+module Rules = struct
+	module Init = struct
+		include struct
+		  open Snarky_backendless.Request
 
-include struct
-  open Snarky_backendless.Request
+		  type _ t +=
+			  | Public_key : Public_key.Compressed.t Snarky_backendless.Request.t
+			  | Token_id : Token_id.t Snarky_backendless.Request.t
+			  | May_use_token : Account_update.May_use_token.t Snarky_backendless.Request.t
+		end
 
-  type _ t +=
-    | Txn_snark : Transaction_snark.Statement.With_sok.t t (* SoK not needed now, might be needed later *)
-    | Txn_snark_proof : (Nat.N2.n, Nat.N2.n) Pickles.Proof.t t
+		module Witness = struct
+		  type t =
+			  { public_key : Public_key.Compressed.t
+			  ; token_id : Token_id.t
+			  ; may_use_token : Account_update.May_use_token.t
+		    }
+		end
+
+		let handler (w : Witness.t)
+		    (Snarky_backendless.Request.With { request; respond }) =
+		  match request with
+		  | Public_key ->
+					respond (Provide w.public_key)
+		  | Token_id ->
+					respond (Provide w.token_id)
+		  | May_use_token ->
+					respond (Provide w.may_use_token)
+		  | _ ->
+		      respond Unhandled
+
+		let main input =
+      let public_key =
+        exists Public_key.Compressed.typ ~request:(fun () -> Public_key)
+      in
+      let token_id = exists Token_id.typ ~request:(fun () -> Token_id) in
+		  Zkapps_examples.wrap_main ~public_key ~token_id
+		    (fun account_update -> account_update#assert_state_unproved )
+				input
+
+		let rule : _ Pickles.Inductive_rule.t =
+		  { identifier = "Rollup init"
+		  ; prevs = []
+		  ; main
+		  ; feature_flags = Pickles_types.Plonk_types.Features.none_bool
+		  }
+	end
+	module Step = struct
+		include struct
+		  open Snarky_backendless.Request
+
+		  type _ t +=
+		    | Txn_snark : Transaction_snark.Statement.With_sok.t t (* SoK not needed now, might be needed later *)
+		    | Txn_snark_proof : (Nat.N2.n, Nat.N2.n) Pickles.Proof.t t
+			  | Public_key : Public_key.Compressed.t Snarky_backendless.Request.t
+			  | Token_id : Token_id.t Snarky_backendless.Request.t
+			  | May_use_token : Account_update.May_use_token.t Snarky_backendless.Request.t
+		end
+
+		module Witness = struct
+		  type t =
+		    { txn_snark : Transaction_snark.Statement.With_sok.t
+		    ; txn_snark_proof : (Nat.N2.n, Nat.N2.n) Pickles.Proof.t
+			  ; public_key : Public_key.Compressed.t
+			  ; token_id : Token_id.t
+			  ; may_use_token : Account_update.May_use_token.t
+		    }
+		end
+
+		let handler (w : Witness.t)
+		    (Snarky_backendless.Request.With { request; respond }) =
+		  match request with
+		  | Txn_snark ->
+		      respond (Provide w.txn_snark)
+		  | Txn_snark_proof ->
+		      respond (Provide w.txn_snark_proof)
+		  | Public_key ->
+					respond (Provide w.public_key)
+		  | Token_id ->
+					respond (Provide w.token_id)
+		  | May_use_token ->
+					respond (Provide w.may_use_token)
+		  | _ ->
+		      respond Unhandled
+
+		let main input =
+		  let txn_snark =
+		    exists Transaction_snark.Statement.With_sok.typ ~request:(fun () -> Txn_snark)
+		  in
+		  let txn_snark_proof =
+		    exists (Typ.Internal.ref ()) ~request:(fun () -> Txn_snark_proof)
+		  in
+      let public_key =
+        exists Public_key.Compressed.typ ~request:(fun () -> Public_key)
+      in
+      let token_id = exists Token_id.typ ~request:(fun () -> Token_id) in
+		  let prev_state = txn_snark.source.first_pass_ledger in
+		  let prev_state_raw = Frozen_ledger_hash0.var_to_field prev_state in
+		  let next_state = txn_snark.target.second_pass_ledger in
+		  let next_state_raw = Frozen_ledger_hash0.var_to_field next_state in
+		  let () = Field.Assert.equal
+		    (Frozen_ledger_hash0.var_to_field txn_snark.target.first_pass_ledger)
+		    (Frozen_ledger_hash0.var_to_field txn_snark.source.second_pass_ledger) in
+		  let { previous_proof_statements = _ ; public_output = account_update ; auxiliary_output = () } =
+		    Zkapps_examples.wrap_main ~public_key ~token_id
+		      (fun account_update ->
+		        account_update#assert_state_proved ;
+		        account_update#set_prev_state 0 prev_state_raw ;
+		        account_update#set_state 0 next_state_raw ;
+		        ()
+		      )
+		      input
+		  in
+		  let open Pickles.Inductive_rule in
+		  { previous_proof_statements =
+		    [ { public_input = txn_snark
+		      ; proof_must_verify = Boolean.true_
+		      ; proof = txn_snark_proof
+		      }
+		    ]
+		  ; public_output = account_update
+		  ; auxiliary_output = ()
+		  }
+
+		let rule txn_snark_tag : _ Pickles.Inductive_rule.t =
+		  { identifier = "Rollup step"
+		  ; prevs = [ txn_snark_tag ]
+		  ; main
+		  ; feature_flags = Pickles_types.Plonk_types.Features.none_bool
+		  }
+	end
 end
 
-module Witness = struct
-  type t =
-    { txn_snark : Transaction_snark.Statement.With_sok.t
-    ; txn_snark_proof : (Nat.N2.n, Nat.N2.n) Pickles.Proof.t
-    }
+module Make (T : sig val tag : Transaction_snark.tag end) = struct
+	let tag, cache_handle, p, Pickles.Provers.[ init ; step ] =
+	    (Zkapps_examples.compile () ~cache:Cache_dir.cache
+	       ~auxiliary_typ:Impl.Typ.unit
+	       ~branches:(module Nat.N0)
+	       ~max_proofs_verified:(module Nat.N1)
+	       ~name:"rollup"
+	       ~constraint_constants:
+	         Genesis_constants.Constraint_constants.(to_snark_keys_header compiled)
+	       ~choices:(fun ~self ->
+	         [ Rules.Init.rule
+           ; Rules.Step.rule T.tag
+	         ] ) )
+
+	let vk = Pickles.Side_loaded.Verification_key.of_compiled tag
+
+  module Proof = (val p)
 end
-
-let update_state_handler (w : Witness.t)
-    (Snarky_backendless.Request.With { request; respond }) =
-  match request with
-  | Txn_snark ->
-      respond (Provide w.txn_snark)
-  | Txn_snark_proof ->
-      respond (Provide w.txn_snark_proof)
-  | _ ->
-      respond Unhandled
-
-let update_state public_key input =
-  let txn_snark =
-    exists Transaction_snark.Statement.With_sok.typ ~request:(fun () -> Txn_snark)
-  in
-  let txn_snark_proof =
-    exists (Typ.Internal.ref ()) ~request:(fun () -> Txn_snark_proof)
-  in
-  let prev_state = txn_snark.source.first_pass_ledger in
-  let prev_state_raw = Frozen_ledger_hash0.var_to_field prev_state in
-  let next_state = txn_snark.target.second_pass_ledger in
-  let next_state_raw = Frozen_ledger_hash0.var_to_field next_state in
-  let () = Field.Assert.equal
-    (Frozen_ledger_hash0.var_to_field txn_snark.target.first_pass_ledger)
-    (Frozen_ledger_hash0.var_to_field txn_snark.source.second_pass_ledger) in
-  let { previous_proof_statements = _ ; public_output = account_update ; auxiliary_output = () } =
-    Zkapps_examples.wrap_main
-      ~public_key:(Public_key.Compressed.var_of_t public_key)
-      (fun account_update ->
-        account_update#assert_state_proved ;
-        account_update#set_prev_state 0 prev_state_raw ;
-        account_update#set_state 0 next_state_raw ;
-        ()
-      )
-      input
-  in
-  let open Pickles.Inductive_rule in
-  { previous_proof_statements =
-    [ { public_input = txn_snark
-      ; proof_must_verify = Boolean.true_
-      ; proof = txn_snark_proof
-      }
-    ]
-  ; public_output = account_update
-  ; auxiliary_output = ()
-  }
-
-let initialize_rule public_key : _ Pickles.Inductive_rule.t =
-  { identifier = "Initialise rollup"
-  ; prevs = []
-  ; main = initialize public_key
-  ; feature_flags = Pickles_types.Plonk_types.Features.none_bool
-  }
-
-let update_state_rule txn_snark_tag public_key : _ Pickles.Inductive_rule.t =
-  { identifier = "Update state"
-  ; prevs = [ txn_snark_tag ]
-  ; main = update_state public_key
-  ; feature_flags = Pickles_types.Plonk_types.Features.none_bool
-  }
