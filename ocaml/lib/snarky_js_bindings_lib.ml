b06ee30f6967323d464a85354e7e4743393ced22
Las Safin <me@las.rs>
2023-08-22 13:54:24 +0000

    Export Async_js.init

--- ocaml/lib/snarky_js_bindings_lib.ml
+++ ocaml/lib/snarky_js_bindings_lib.ml
@@ -1,11 +1,16 @@
 module Js = Js_of_ocaml.Js
 
+let async_js = object%js
+  method init = Async_js.init ()
+end
+
 let export () =
   Js.export "Snarky" Snarky_bindings.snarky ;
   Js.export "Ledger" Local_ledger.ledger_class ;
   Js.export "Pickles" Pickles_bindings.pickles ;
   Js.export "Test" Consistency_test.test ;
-  Js.export "Rollup" Rollup.rollup
+  Js.export "Rollup" Rollup.rollup ;
+  Js.export "Async_js" async_js
 
 let export_global () =
   let snarky_obj =
@@ -17,6 +22,7 @@ let export_global () =
          ; ("Pickles", i Pickles_bindings.pickles)
          ; ("Test", i Consistency_test.test)
          ; ("Rollup", i Rollup.rollup)
+         ; ("Async_js", i async_js)
         |])
   in
   Js.Unsafe.(set global (Js.string "__snarky") snarky_obj)
