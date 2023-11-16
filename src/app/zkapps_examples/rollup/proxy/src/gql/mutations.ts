88f4d9c1fe362b82b1788960aa8aaa95b04c0603
Martin Ondejka <ondejka99@gmail.com>
2023-11-16 19:04:19 +0100

    Remove ts gql

--- src/app/zkapps_examples/rollup/proxy/src/gql/mutations.ts
+++
@@ -1,8 +0,0 @@
-import { GraphQLError } from "graphql";
-import { MutationResolvers } from "../generated/graphql";
-
-export const mutations: MutationResolvers = {
-  async sendPayment(_, { input, signature }) {
-    throw new GraphQLError("Not implemented");
-  },
-};
