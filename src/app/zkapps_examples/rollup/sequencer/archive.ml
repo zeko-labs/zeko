f492ae4e28cb99d0dc4207f0d61bda41970b0155
Martin Ondejka <ondejka99@gmail.com>
2023-11-22 22:07:32 +0100

    Add events storing

--- src/app/zkapps_examples/rollup/sequencer/archive.ml
+++ src/app/zkapps_examples/rollup/sequencer/archive.ml
@@ -8,6 +8,7 @@ module Block_info = struct
     ; state_hash : string
     ; parent_hash : string
     ; ledger_hash : string
+    ; chain_status : string
     ; timestamp : int
     ; global_slot_since_hardfork : int
     ; global_slot_since_genesis : int
@@ -19,6 +20,7 @@ module Block_info = struct
     ; state_hash = ""
     ; parent_hash = ""
     ; ledger_hash = ""
+    ; chain_status = ""
     ; timestamp = 0
     ; global_slot_since_hardfork = 0
     ; global_slot_since_genesis = 0
@@ -35,9 +37,19 @@ module Transaction_info = struct
     }
 end
 
-module Action = struct
+module Event = struct
   type t = Snark_params.Tick.Field.t array
 end
+
+module Account_update_events = struct
+  type t =
+    { block_info : Block_info.t option
+    ; transaction_info : Transaction_info.t option
+    ; events : Event.t list
+    }
+end
+
+module Action = Event
 
 module Account_update_actions = struct
   type t =
@@ -50,11 +62,16 @@ module Account_update_actions = struct
 end
 
 module Archive = struct
-  type t = { mutable actions_map : Account_update_actions.t list Account_map.t }
+  type t =
+    { mutable actions_map : Account_update_actions.t list Account_map.t
+    ; mutable events_map : Account_update_events.t list Account_map.t
+    }
 
-  let create () = { actions_map = Account_map.empty }
+  let create () =
+    { actions_map = Account_map.empty; events_map = Account_map.empty }
 
-  let add_actions t (account_update : Account_update.t) (account : Account.t) =
+  let add_actions t (account_update : Account_update.t) transaction_info
+      (account : Account.t) =
     match account_update.body.actions with
     | [] ->
         ()
@@ -64,7 +81,7 @@ module Archive = struct
         in
         let action =
           { Account_update_actions.block_info = Some Block_info.dummy
-          ; transaction_info = None
+          ; transaction_info
           ; action_state = zkapp.action_state
           ; account_update_id = 0
           ; actions
@@ -80,8 +97,30 @@ module Archive = struct
         t.actions_map <-
           Account_map.add account (action :: previous) t.actions_map
 
-  let add_account_update t (account_update : Account_update.t) =
-    add_actions t account_update
+  let add_events t (account_update : Account_update.t) transaction_info =
+    match account_update.body.events with
+    | [] ->
+        ()
+    | events ->
+        let event =
+          { Account_update_events.block_info = Some Block_info.dummy
+          ; transaction_info
+          ; events
+          }
+        in
+        let account : Account_id.t =
+          Account_id.create account_update.body.public_key
+            account_update.body.token_id
+        in
+        let previous =
+          Option.value ~default:[] (Account_map.find_opt account t.events_map)
+        in
+        t.events_map <- Account_map.add account (event :: previous) t.events_map
+
+  let add_account_update t (account_update : Account_update.t) account
+      transaction_info =
+    add_actions t account_update transaction_info account ;
+    add_events t account_update transaction_info
 
   let get_actions t account_id from =
     let open Account_update_actions in
@@ -103,6 +142,10 @@ module Archive = struct
     (* If the `from` wasn't found return all *)
     (* Weird, but it's the same way in archive node *)
     match filter_start from all with [] -> all | actions -> actions
+
+  let get_events t account_id =
+    List.rev
+    @@ Option.value ~default:[] (Account_map.find_opt account_id t.events_map)
 end
 
 include Archive
