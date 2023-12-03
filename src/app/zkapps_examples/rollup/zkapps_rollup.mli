26edb84c28c026ef237bc55f3d09adb1ed1fc8aa
Las Safin <me@las.rs>
2023-12-03 21:06:42 +0000

    Add comments and expose function for submitting actions

--- src/app/zkapps_examples/rollup/zkapps_rollup.mli
+++ src/app/zkapps_examples/rollup/zkapps_rollup.mli
@@ -1,11 +1,13 @@
 open Core_kernel
 open Async_kernel
+open Mina_base
+open Signature_lib
 
 type t
 
-val source_ledger : t -> Mina_base.Frozen_ledger_hash.t
+val source_ledger : t -> Frozen_ledger_hash.t
 
-val target_ledger : t -> Mina_base.Frozen_ledger_hash.t
+val target_ledger : t -> Frozen_ledger_hash.t
 
 module Make (T : sig
   val tag : Transaction_snark.tag
@@ -14,5 +16,33 @@ end) : sig
     val wrap : Transaction_snark.t -> t Deferred.t
 
     val merge : t -> t -> t Deferred.t
+  end
+
+  module Inner : sig
+    val vk : Pickles.Side_loaded.Verification_key.t
+
+    val action :
+         public_key:Public_key.Compressed.t
+      -> amount:Currency.Amount.t
+      -> recipient:Public_key.Compressed.t
+      -> ( Account_update.t
+         , Zkapp_command.Digest.Account_update.t
+         , Zkapp_command.Digest.Forest.t )
+         Zkapp_command.Call_forest.Tree.t
+         Deferred.t
+  end
+
+  module Outer : sig
+    val vk : Pickles.Side_loaded.Verification_key.t
+
+    val action :
+         public_key:Public_key.Compressed.t
+      -> amount:Currency.Amount.t
+      -> recipient:Public_key.Compressed.t
+      -> ( Account_update.t
+         , Zkapp_command.Digest.Account_update.t
+         , Zkapp_command.Digest.Forest.t )
+         Zkapp_command.Call_forest.Tree.t
+         Deferred.t
   end
 end
