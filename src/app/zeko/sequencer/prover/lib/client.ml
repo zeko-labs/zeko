d81064bc0f6e158e4e0a4283f247e6da0fdf87bc
Martin Ondejka <ondejka99@gmail.com>
2025-01-06 15:32:10 +0100

    Integrate zeko_transaction_snark into the prover/client

--- src/app/zeko/sequencer/prover/lib/client.ml
+++ src/app/zeko/sequencer/prover/lib/client.ml
@@ -1,5 +1,6 @@
 open Async
 open Core_kernel
+open Zeko_circuits
 
 let try_connect where_to_connect =
   match%bind try_with (fun () -> Tcp.connect where_to_connect) with
@@ -94,88 +95,97 @@ let rec send ?(proving_timeout = 20.) ?(attempts = 5) ?(cooldown = 2.) t
       in
       go ~attempts )
 
-let wrapper_wrap ?proving_timeout t ~txn_snark =
-  send ?proving_timeout t (Prover.Input.Wrapper_wrap txn_snark)
-  >>| function
-  | Prover.Output.Wrapper_wrap x -> x | _ -> failwith "Unexpected response"
-
-let wrapper_merge ?proving_timeout t a b =
-  send ?proving_timeout t (Prover.Input.Wrapper_merge (a, b))
-  >>| function
-  | Prover.Output.Wrapper_merge x -> x | _ -> failwith "Unexpected response"
-
-let transaction_snark_of_signed_command ?proving_timeout t ~statement
-    ~user_command_in_block ~sparse_ledger =
+let transaction_snark_of_single_signed_command ?proving_timeout t ~source_ledger
+    ~source_acc_set ~sequencer_pk ~command ~sparse_ledger =
   send ?proving_timeout t
-    (Prover.Input.Transaction_snark_of_signed_command
-       (statement, user_command_in_block, sparse_ledger) )
+    (Prover.Input.Txn_snark_single_signed_command
+       (source_ledger, source_acc_set, sequencer_pk, command, sparse_ledger) )
   >>| function
-  | Prover.Output.Transaction_snark_of_signed_command x ->
-      x
+  | Prover.Output.Zeko_transaction_snark (stmt, proof) ->
+      Zeko_transaction_snark.make_unchecked ~proof stmt
   | _ ->
-      failwith "Unexpected response"
+      failwith "Unexpected response from prover"
 
-let transaction_snark_of_zkapp_command_segment ?proving_timeout t ~statement
-    ~witness ~spec =
+let transaction_snark_of_single_unproved_zkapp_command ?proving_timeout t
+    ~source_ledger ~target_ledger ~connecting_ledger ~source_local_state
+    ~target_local_state ~fee_excess ~supply_decrease ~txn_snark_witness
+    ~sequencer ~source_acc_set ~shift_action_state =
   send ?proving_timeout t
-    (Prover.Input.Transaction_snark_of_zkapp_command_segment
-       (statement, witness, spec) )
+    (Prover.Input.Txn_snark_single_unproved_zkapp_command
+       ( source_ledger
+       , target_ledger
+       , connecting_ledger
+       , source_local_state
+       , target_local_state
+       , fee_excess
+       , supply_decrease
+       , txn_snark_witness
+       , sequencer
+       , source_acc_set
+       , shift_action_state ) )
   >>| function
-  | Prover.Output.Transaction_snark_of_zkapp_command_segment x ->
-      x
+  | Prover.Output.Zeko_transaction_snark (stmt, proof) ->
+      Zeko_transaction_snark.make_unchecked ~proof stmt
   | _ ->
-      failwith "Unexpected response"
+      failwith "Unexpected response from prover"
 
-let transaction_snark_merge ?proving_timeout t a b =
-  send ?proving_timeout t (Prover.Input.Transaction_snark_merge (a, b))
+let transaction_snark_of_double_unproved_zkapp_command ?proving_timeout t
+    ~source_ledger ~target_ledger ~connecting_ledger ~source_local_state
+    ~target_local_state ~fee_excess ~supply_decrease ~txn_snark_witness
+    ~sequencer ~source_acc_set ~shift_action_state_first
+    ~shift_action_state_second =
+  send ?proving_timeout t
+    (Prover.Input.Txn_snark_double_unproved_zkapp_command
+       ( source_ledger
+       , target_ledger
+       , connecting_ledger
+       , source_local_state
+       , target_local_state
+       , fee_excess
+       , supply_decrease
+       , txn_snark_witness
+       , sequencer
+       , source_acc_set
+       , shift_action_state_first
+       , shift_action_state_second ) )
   >>| function
-  | Prover.Output.Transaction_snark_merge x ->
-      x
+  | Prover.Output.Zeko_transaction_snark (stmt, proof) ->
+      Zeko_transaction_snark.make_unchecked ~proof stmt
   | _ ->
-      failwith "Unexpected response"
+      failwith "Unexpected response from prover"
 
-let submit_deposit ?proving_timeout t ~outer_pk ~deposit =
-  send ?proving_timeout t (Prover.Input.Submit_deposit (outer_pk, deposit))
-  >>| function
-  | Prover.Output.Submit_deposit x -> x | _ -> failwith "Unexpected response"
-
-let submit_withdrawal ?proving_timeout t ~withdrawal =
-  send ?proving_timeout t (Prover.Input.Submit_withdrawal withdrawal)
-  >>| function
-  | Prover.Output.Submit_withdrawal x -> x | _ -> failwith "Unexpected response"
-
-let process_deposit ?proving_timeout t ~is_new ~pointer ~before ~after ~deposit
-    =
+let transaction_snark_of_single_proved_zkapp_command ?proving_timeout t
+    ~source_ledger ~target_ledger ~connecting_ledger ~source_local_state
+    ~target_local_state ~fee_excess ~supply_decrease ~txn_snark_witness
+    ~sequencer ~source_acc_set ~zkapp_vk ~zkapp_proof ~shift_action_state =
   send ?proving_timeout t
-    (Prover.Input.Process_deposit (is_new, pointer, before, after, deposit))
+    (Prover.Input.Txn_snark_single_proved_zkapp_command
+       ( source_ledger
+       , target_ledger
+       , connecting_ledger
+       , source_local_state
+       , target_local_state
+       , fee_excess
+       , supply_decrease
+       , txn_snark_witness
+       , sequencer
+       , source_acc_set
+       , zkapp_vk
+       , zkapp_proof
+       , shift_action_state ) )
   >>| function
-  | Prover.Output.Process_deposit x -> x | _ -> failwith "Unexpected response"
-
-let process_withdrawal ?proving_timeout t ~outer_pk ~is_new ~pointer ~before
-    ~after ~withdrawal =
-  send ?proving_timeout t
-    (Prover.Input.Process_withdrawal
-       (outer_pk, is_new, pointer, before, after, withdrawal) )
-  >>| function
-  | Prover.Output.Process_withdrawal x ->
-      x
+  | Prover.Output.Zeko_transaction_snark (stmt, proof) ->
+      Zeko_transaction_snark.make_unchecked ~proof stmt
   | _ ->
-      failwith "Unexpected response"
+      failwith "Unexpected response from prover"
 
-let outer_step ?proving_timeout t ~last ~outer_public_key ~new_deposits
-    ~unprocessed_deposits ~old_inner_ledger ~new_inner_ledger =
+let transaction_snark_of_merge ?proving_timeout t
+    ~(left : Zeko_transaction_snark.T.t) ~(right : Zeko_transaction_snark.T.t) =
   send ?proving_timeout t
-    (Prover.Input.Outer_step
-       ( last
-       , outer_public_key
-       , new_deposits
-       , unprocessed_deposits
-       , old_inner_ledger
-       , new_inner_ledger ) )
+    (Prover.Input.Txn_snark_merge
+       (left.stmt, left.proof, right.stmt, right.proof) )
   >>| function
-  | Prover.Output.Outer_step x -> x | _ -> failwith "Unexpected response"
-
-let inner_step ?proving_timeout t ~all_deposits =
-  send ?proving_timeout t (Prover.Input.Inner_step all_deposits)
-  >>| function
-  | Prover.Output.Inner_step x -> x | _ -> failwith "Unexpected response"
+  | Prover.Output.Zeko_transaction_snark (stmt, proof) ->
+      Zeko_transaction_snark.make_unchecked ~proof stmt
+  | _ ->
+      failwith "Unexpected response from prover"
