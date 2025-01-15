657218cad1476ae801114b529863627b2c972498
Martin Ondejka <ondejka99@gmail.com>
2025-01-15 18:25:23 +0100

    Clean up snark queue into separate file

--- src/app/zeko/sequencer/lib/transfer.ml
+++ src/app/zeko/sequencer/lib/transfer.ml
@@ -1,12 +1,25 @@
+open Mina_base
+module Field = Snark_params.Tick.Field
+
+type direction = Deposit | Withdraw
+
+type t = { transfer : Zkapps_rollup.TR.t; direction : direction }
+
+type claim =
+  { is_new : bool
+  ; pointer : Field.t
+  ; before : Zkapps_rollup.TR.t list
+  ; after : Zkapps_rollup.TR.t list
+  ; transfer : t
+  }
+
 (**
   Hash table that holds the item only for the specified lifetime.
   Used to store proved transfer requested by users.
 *)
-
-open Base
-open Mina_base
-
 module Transfers_memory = struct
+  open Base
+
   type t_ =
     ( ( Account_update.t
       , Zkapp_command.Digest.Account_update.t
@@ -48,5 +61,3 @@ module Transfers_memory = struct
 
   let get t key = Hashtbl.find t.table key
 end
-
-include Transfers_memory
--- src/app/zeko/sequencer/lib/zeko_sequencer.ml
+++ src/app/zeko/sequencer/lib/zeko_sequencer.ml
@@ -28,20 +28,6 @@ module Sequencer = struct
       }
   end
 
-  module Transfer = struct
-    type direction = Deposit | Withdraw
-
-    type t = { transfer : Zkapps_rollup.TR.t; direction : direction }
-
-    type claim =
-      { is_new : bool
-      ; pointer : Field.t
-      ; before : Zkapps_rollup.TR.t list
-      ; after : Zkapps_rollup.TR.t list
-      ; transfer : t
-      }
-  end
-
   let genesis_constants = Genesis_constants.Compiled.genesis_constants
 
   let compile_time_genesis_state =
@@ -64,91 +50,6 @@ module Sequencer = struct
     Sok_message.digest
     @@ Sok_message.create ~fee:Currency.Fee.zero
          ~prover:(Public_key.compress keypair.public_key)
-
-  module Snark_queue = struct
-    type t =
-      { q : unit Throttle.t
-      ; config : Config.t
-      ; transfers_memory : Transfers_memory.t
-      ; provers : Zeko_prover.Client.t
-      }
-
-    let create ~config ~provers =
-      { q = Throttle.create ~continue_on_error:false ~max_concurrent_jobs:1
-      ; config
-      ; transfers_memory = Transfers_memory.create ~lifetime:Float.(60. * 10.)
-      ; provers
-      }
-
-    let queue_size t = Throttle.num_jobs_waiting_to_start t.q
-
-    let enqueue t f =
-      Throttle.enqueue t.q (fun () ->
-          let%map result = f () in
-          result )
-
-    let enqueue_prove_transfer_request t ~key ~(transfer : Transfer.t) =
-      Throttle.enqueue t.q (fun () ->
-          let%bind result =
-            try_with (fun () ->
-                match transfer with
-                | { direction = Deposit; transfer } ->
-                    Zeko_prover.Client.submit_deposit t.provers
-                      ~outer_pk:t.config.zkapp_pk ~deposit:transfer
-                | { direction = Withdraw; transfer } ->
-                    Zeko_prover.Client.submit_withdrawal t.provers
-                      ~withdrawal:transfer )
-          in
-          let () =
-            match result with
-            | Ok tree ->
-                Transfers_memory.add t.transfers_memory key
-                  (Ok (Zkapp_command.Call_forest.cons_tree tree []))
-            | Error e ->
-                printf "Warning: prove_transfer_request failed %s\n%!"
-                  (Exn.to_string e) ;
-                Transfers_memory.add t.transfers_memory key
-                  (Error (Exn.to_string e))
-          in
-          return () )
-
-    let enqueue_prove_transfer_claim t ~key ~(claim : Transfer.claim) =
-      Throttle.enqueue t.q (fun () ->
-          let%bind result =
-            try_with (fun () ->
-                match claim with
-                | { transfer = { direction = Deposit; transfer }
-                  ; is_new
-                  ; pointer
-                  ; before
-                  ; after
-                  } ->
-                    Zeko_prover.Client.process_deposit t.provers ~is_new
-                      ~pointer ~before ~after ~deposit:transfer
-                | { transfer = { direction = Withdraw; transfer }
-                  ; is_new
-                  ; pointer
-                  ; before
-                  ; after
-                  } ->
-                    Zeko_prover.Client.process_withdrawal t.provers
-                      ~outer_pk:t.config.zkapp_pk ~is_new ~pointer ~before
-                      ~after ~withdrawal:transfer )
-          in
-          let () =
-            match result with
-            | Ok forest ->
-                Transfers_memory.add t.transfers_memory key (Ok forest)
-            | Error e ->
-                printf "Warning: prove_transfer_claim failed %s\n%!"
-                  (Exn.to_string e) ;
-                Transfers_memory.add t.transfers_memory key
-                  (Error (Exn.to_string e))
-          in
-          return () )
-
-    let wait_to_finish t = Throttle.capacity_available t.q
-  end
 
   module Merger = struct
     module Command_witness = struct
@@ -251,13 +152,6 @@ module Sequencer = struct
     end
 
     module Commit = struct
-      (* Only for yojson serialization of Field *)
-      module Field = Data_hash.Make_full_size (struct
-        let description = "Field"
-
-        let version_byte = '\x00'
-      end)
-
       type t =
         { new_inner_ledger : Sparse_ledger.t
         ; old_deposits_pointer : Field.t
