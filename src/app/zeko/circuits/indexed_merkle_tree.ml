a772830f277fe22fdb3b9d478b6fbaf8da7ef59f
Las <me@las.rs>
2025-01-16 13:02:33 +0000

    Fix bug in indexed merkle tree, found in audit
    `root_new` was completely unconstrained when `check` was `false`.

--- src/app/zeko/circuits/indexed_merkle_tree.ml
+++ src/app/zeko/circuits/indexed_merkle_tree.ml
@@ -66,5 +66,12 @@ struct
       assert_equal ~label:__LOC__ F.typ root_intermediate root_intermediate'
     in
     let* root_new = implied_root { key = y; next_key = z } path_y in
+    let* root_new =
+      match check with
+      | Some check ->
+          if_ check ~typ:F.typ ~then_:root_new ~else_:root
+      | None ->
+          Checked.return root_new
+    in
     Checked.return (`Before_adding_y root, `After_adding_y root_new)
 end
