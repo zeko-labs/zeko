cf576848a05336d8287ef36d188b60485df45aa9
Martin Ondejka <ondejka99@gmail.com>
2025-02-11 19:43:14 +0000

    Refactor logging in archive relay

--- src/app/zeko/sequencer/archive_relay/run.ml
+++ src/app/zeko/sequencer/archive_relay/run.ml
@@ -146,10 +146,15 @@ let sync_archive (t : t) ~hash =
           with
           | Ok () ->
               [%log info]
-                "Synced diff to archive with hash: %s, progress %.0f%%"
-                (Ledger_hash.to_decimal_string @@ Ledger.merkle_root ledger)
+                "Synced diff to archive with hash: $hash, progress %.0f%%"
                 ( Float.of_int current_chunk /. Float.of_int chunks_length
-                *. 100.0 ) ;
+                *. 100.0 )
+                ~metadata:
+                  [ ( "hash"
+                    , `String
+                        ( Ledger_hash.to_decimal_string
+                        @@ Ledger.merkle_root ledger ) )
+                  ] ;
               return ()
           | Error e ->
               raise (Error.to_exn e) ) )
@@ -212,8 +217,9 @@ let sync (t : t) () =
       let%bind ledger_hash =
         match%bind fetch_current_ledger_hash ~zeko_uri:t.zeko_uri () with
         | Ok hash ->
-            [%log info] "Fetched ledger hash: %s"
-              (Ledger_hash.to_decimal_string hash) ;
+            [%log info] "Fetched ledger hash: $hash"
+              ~metadata:
+                [ ("hash", `String (Ledger_hash.to_decimal_string hash)) ] ;
             return hash
         | Error e ->
             failwith e
@@ -230,7 +236,8 @@ let rec run (t : t) ~sync_period () =
             after (Time.Span.of_sec sync_period) )
     | Error e ->
         (* ledger_hash_invalidated *)
-        [%log error] "Error syncing: %s" (Error.to_string_hum e) ;
+        [%log error] "Error syncing: $error"
+          ~metadata:[ ("error", `String (Error.to_string_hum e)) ] ;
         [%log warn] "Invalidating ledger cache" ;
         reset_ledger_cache t ()
   in
