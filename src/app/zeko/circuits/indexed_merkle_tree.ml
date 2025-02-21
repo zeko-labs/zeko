f3d20a266d018d48483f621f14e5be91d60fd43b
Las <me@las.rs>
2025-02-21 11:40:06 +0000

    Fix indexed merkle tree add_key_var when check = false

--- src/app/zeko/circuits/indexed_merkle_tree.ml
+++ src/app/zeko/circuits/indexed_merkle_tree.ml
@@ -35,10 +35,14 @@ struct
 
   let hash_entry = var_to_hash ~init:"indexed merkle tree entry hash" Entry.typ
 
+  (* TODO: consider different salt per level. *)
   let implied_root_raw (init : F.var) (path : Path.var) : F.var Checked.t =
     Checked.List.fold path ~init ~f:(fun acc { hash; is_left } ->
-        let* left = if_ is_left ~typ:F.typ ~then_:hash ~else_:acc in
-        let* right = if_ is_left ~typ:F.typ ~then_:acc ~else_:hash in
+        let* left, right =
+          if_ is_left
+            ~typ:Typ.(F.typ * F.typ)
+            ~then_:(hash, acc) ~else_:(acc, hash)
+        in
         var_to_hash ~init:"indexed merkle tree" Typ.(F.typ * F.typ) (left, right) )
 
   let implied_root (entry : Entry.var) (path : Path.var) : F.var Checked.t =
@@ -54,13 +58,13 @@ struct
     let* root_intermediate' =
       implied_root_raw Field.(constant typ zero) path_y
     in
-    let* root_intermediate' =
+    let* root_intermediate =
       match check with
       | Some check ->
-          if_ check ~typ:F.typ ~then_:root_intermediate'
-            ~else_:root_intermediate
+          if_ check ~typ:F.typ ~then_:root_intermediate
+            ~else_:root_intermediate'
       | None ->
-          Checked.return root_intermediate'
+          Checked.return root_intermediate
     in
     let* () =
       assert_equal ~label:__LOC__ F.typ root_intermediate root_intermediate'
