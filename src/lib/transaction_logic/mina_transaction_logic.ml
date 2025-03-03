0f1c268c1ee093788d6bf58904aa692aa8e55995
Martin Ondejka <ondejka99@gmail.com>
2025-03-03 16:34:41 +0000

    tmp

--- src/lib/transaction_logic/mina_transaction_logic.ml
+++ src/lib/transaction_logic/mina_transaction_logic.ml
@@ -1308,6 +1308,8 @@ module Make (L : Ledger_intf.S) :
       module Signed = struct
         include Signed
 
+        let display x = sexp_of_t x |> Sexp.to_string_hum
+
         let if_ = value_if
 
         (* Correctness of these functions hinges on the fact that zero is
