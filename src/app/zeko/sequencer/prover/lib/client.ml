5bd9ef0b44465f57f5cfd0e7751d9287d44f8550
Martin Ondejka <ondejka99@gmail.com>
2025-03-08 22:38:44 +0000

    Add all the prover functions into the client

--- src/app/zeko/sequencer/prover/lib/client.ml
+++ src/app/zeko/sequencer/prover/lib/client.ml
@@ -1,7 +1,10 @@
 open Async
 open Core_kernel
-open Zeko_circuits
 open Mina_base
+open Mina_ledger
+open Zeko_circuits
+open Zeko_types
+module Field = Snark_params.Tick.Field
 
 let try_connect where_to_connect =
   match%bind try_with (fun () -> Tcp.connect where_to_connect) with
@@ -96,263 +99,140 @@ let rec send ?(proving_timeout = 20.) ?(attempts = 5) ?(cooldown = 2.) t
       in
       go ~attempts )
 
-let transaction_snark_of_single_signed_command ?proving_timeout t ~source_ledger
-    ~sequencer_pk ~command ~sparse_ledger ~account_set_witness =
-  send ?proving_timeout t
-    (Prover.Input.Txn_snark_single_signed_command
-       (source_ledger, sequencer_pk, command, sparse_ledger, account_set_witness)
-    )
+let transaction_snark ?proving_timeout t input =
+  send ?proving_timeout t (Prover.Input.Txn_snark input)
   >>| function
-  | Prover.Output.Zeko_transaction_snark (stmt, proof) ->
-      ({ stmt; proof } : Zeko_transaction_snark.T.t)
+  | Prover.Output.Txn_snark snark ->
+      Txn_snark.of_serializable snark
   | _ ->
       failwith "Unexpected response from prover"
 
-let transaction_snark_of_single_unproved_zkapp_command ?proving_timeout t
-    ~source_ledger ~target_ledger ~connecting_ledger ~source_local_state
-    ~target_local_state ~fee_excess ~supply_decrease ~txn_snark_witness
-    ~sequencer ~account_set_witness ~shift_action_state =
-  send ?proving_timeout t
-    (Prover.Input.Txn_snark_single_unproved_zkapp_command
-       ( source_ledger
-       , target_ledger
-       , connecting_ledger
-       , source_local_state
-       , target_local_state
-       , fee_excess
-       , supply_decrease
-       , txn_snark_witness
-       , sequencer
-       , shift_action_state
-       , account_set_witness ) )
+let ase_with_length ?proving_timeout t input =
+  send ?proving_timeout t (Prover.Input.Ase (With_length input))
   >>| function
-  | Prover.Output.Zeko_transaction_snark (stmt, proof) ->
-      ({ stmt; proof } : Zeko_transaction_snark.T.t)
+  | Prover.Output.Ase (With_length ase) ->
+      ase
   | _ ->
       failwith "Unexpected response from prover"
 
-let transaction_snark_of_double_unproved_zkapp_command ?proving_timeout t
-    ~source_ledger ~target_ledger ~connecting_ledger ~source_local_state
-    ~target_local_state ~fee_excess ~supply_decrease ~txn_snark_witness
-    ~sequencer ~shift_action_state_first ~shift_action_state_second
-    ~account_set_witness =
-  send ?proving_timeout t
-    (Prover.Input.Txn_snark_double_unproved_zkapp_command
-       ( source_ledger
-       , target_ledger
-       , connecting_ledger
-       , source_local_state
-       , target_local_state
-       , fee_excess
-       , supply_decrease
-       , txn_snark_witness
-       , sequencer
-       , shift_action_state_first
-       , shift_action_state_second
-       , account_set_witness ) )
+let ase_without_length ?proving_timeout t input =
+  send ?proving_timeout t (Prover.Input.Ase (Without_length input))
   >>| function
-  | Prover.Output.Zeko_transaction_snark (stmt, proof) ->
-      ({ stmt; proof } : Zeko_transaction_snark.T.t)
+  | Prover.Output.Ase (Without_length ase) ->
+      ase
   | _ ->
       failwith "Unexpected response from prover"
 
-let transaction_snark_of_single_proved_zkapp_command ?proving_timeout t
-    ~source_ledger ~target_ledger ~connecting_ledger ~source_local_state
-    ~target_local_state ~fee_excess ~supply_decrease ~txn_snark_witness
-    ~sequencer ~zkapp_vk ~zkapp_proof ~shift_action_state ~account_set_witness =
-  send ?proving_timeout t
-    (Prover.Input.Txn_snark_single_proved_zkapp_command
-       ( source_ledger
-       , target_ledger
-       , connecting_ledger
-       , source_local_state
-       , target_local_state
-       , fee_excess
-       , supply_decrease
-       , txn_snark_witness
-       , sequencer
-       , zkapp_vk
-       , zkapp_proof
-       , shift_action_state
-       , account_set_witness ) )
-  >>| function
-  | Prover.Output.Zeko_transaction_snark (stmt, proof) ->
-      ({ stmt; proof } : Zeko_transaction_snark.T.t)
-  | _ ->
-      failwith "Unexpected response from prover"
-
-let transaction_snark_of_merge ?proving_timeout t
-    ~(left : Zeko_transaction_snark.T.t) ~(right : Zeko_transaction_snark.T.t) =
-  send ?proving_timeout t
-    (Prover.Input.Txn_snark_merge
-       (left.stmt, left.proof, right.stmt, right.proof) )
-  >>| function
-  | Prover.Output.Zeko_transaction_snark (stmt, proof) ->
-      ({ stmt; proof } : Zeko_transaction_snark.T.t)
-  | _ ->
-      failwith "Unexpected response from prover"
-
-let transaction_snark_of_segment ?proving_timeout t ~sequencer_pk
-    ~(witness :
-       Transaction_witness.Zkapp_command_segment_witness.t
-       * Transaction_snark.Zkapp_command_segment.Basic.t
-       * Mina_state.Snarked_ledger_state.With_sok.t
-       * Prover.Account_set_witness.t ) =
-  let mina_local_state_to_zeko
-      (t :
-        Mina_transaction_logic.Zkapp_command_logic.Local_state.Value.Stable.V1.t
-        ) : Zeko_transaction_snark.Local_state.t =
-    { ledger = t.ledger
-    ; stack_frame = t.stack_frame
-    ; call_stack = t.call_stack
-    ; transaction_commitment = t.transaction_commitment
-    ; full_transaction_commitment = t.full_transaction_commitment
-    ; excess = t.excess
-    ; account_update_index = t.account_update_index
-    }
+let ase (type target) t ~source ~elems ~max_excess
+    (prover : _ -> _ -> (Compile_simple.Proof.t option * target) Deferred.t) =
+  let elems_to_prove, excess =
+    let i = ref 0 in
+    let l = List.length elems in
+    List.split_while elems ~f:(fun _ ->
+        let r = !i < l - max_excess in
+        i := !i + 1 ;
+        r )
   in
-  let first_account_update
-      (witness : Transaction_witness.Zkapp_command_segment_witness.t) =
-    match witness.local_state_init.stack_frame.calls with
-    | [] ->
-        with_return (fun { return } ->
-            List.iter witness.start_zkapp_command ~f:(fun s ->
-                Zkapp_command.Call_forest.iteri
-                  ~f:(fun _i x -> return (Some x))
-                  s.account_updates.account_updates ) ;
-            None )
-    | xs ->
-        Zkapp_command.Call_forest.hd_account_update xs
-  in
-  let account_update_proof (p : Account_update.t) =
-    match p.authorization with
-    | Proof proof ->
-        Some proof
-    | Signature _ | None_given ->
-        None
-  in
-  let snapp_proof_data
-      ~(witness : Transaction_witness.Zkapp_command_segment_witness.t) =
-    let open Option.Let_syntax in
-    let%bind p = first_account_update witness in
-    let%map pi = account_update_proof p in
-    let vk =
-      let account_id = Account_id.create p.body.public_key p.body.token_id in
-      let account : Account.t =
-        Mina_ledger.Sparse_ledger.(
-          get_exn witness.local_state_init.ledger
-            (find_index_exn witness.local_state_init.ledger account_id))
-      in
-      match
-        Option.value_map ~default:None account.zkapp ~f:(fun s ->
-            s.verification_key )
-      with
-      | None ->
-          failwith "No verification key found in the account"
-      | Some s ->
-          s
-    in
-    (pi, vk)
-  in
-  match witness with
-  | witness, Opt_signed, stmt, account_set_witness ->
-      transaction_snark_of_single_unproved_zkapp_command ?proving_timeout t
-        ~source_ledger:stmt.source.first_pass_ledger
-        ~target_ledger:stmt.target.second_pass_ledger
-        ~connecting_ledger:stmt.connecting_ledger_left (* left or right? *)
-        ~source_local_state:(mina_local_state_to_zeko stmt.source.local_state)
-        ~target_local_state:(mina_local_state_to_zeko stmt.target.local_state)
-        ~fee_excess:stmt.fee_excess.fee_excess_l
-        ~supply_decrease:stmt.supply_increase.magnitude
-        ~txn_snark_witness:witness ~sequencer:sequencer_pk
-        ~shift_action_state:true ~account_set_witness
-  | witness, Opt_signed_opt_signed, stmt, account_set_witness ->
-      transaction_snark_of_double_unproved_zkapp_command ?proving_timeout t
-        ~source_ledger:stmt.source.first_pass_ledger
-        ~target_ledger:stmt.target.second_pass_ledger
-        ~connecting_ledger:stmt.connecting_ledger_left (* left or right? *)
-        ~source_local_state:(mina_local_state_to_zeko stmt.source.local_state)
-        ~target_local_state:(mina_local_state_to_zeko stmt.target.local_state)
-        ~fee_excess:stmt.fee_excess.fee_excess_l
-        ~supply_decrease:stmt.supply_increase.magnitude
-        ~txn_snark_witness:witness ~sequencer:sequencer_pk
-        ~shift_action_state_first:true ~shift_action_state_second:true
-        ~account_set_witness
-  | witness, Proved, stmt, account_set_witness -> (
-      match snapp_proof_data ~witness with
-      | None ->
-          failwith "of_zkapp_command_segment: Expected exactly one proof"
-      | Some (p, v) ->
-          transaction_snark_of_single_proved_zkapp_command ?proving_timeout t
-            ~source_ledger:stmt.source.first_pass_ledger
-            ~target_ledger:stmt.target.second_pass_ledger
-            ~connecting_ledger:stmt.connecting_ledger_left (* left or right? *)
-            ~source_local_state:
-              (mina_local_state_to_zeko stmt.source.local_state)
-            ~target_local_state:
-              (mina_local_state_to_zeko stmt.target.local_state)
-            ~fee_excess:stmt.fee_excess.fee_excess_l
-            ~supply_decrease:stmt.supply_increase.magnitude
-            ~txn_snark_witness:witness ~sequencer:sequencer_pk ~zkapp_vk:v.data
-            ~zkapp_proof:(Compile_simple.Proof.of_pickles p)
-            ~shift_action_state:true ~account_set_witness )
-
-let transaction_snark_of_zkapp_command ?proving_timeout t ~sequencer_pk
-    ~(witnesses :
-       ( Transaction_witness.Zkapp_command_segment_witness.t
-       * Transaction_snark.Zkapp_command_segment.Basic.t
-       * Mina_state.Snarked_ledger_state.With_sok.t
-       * Prover.Account_set_witness.t )
-       list ) =
-  match witnesses with
+  match elems_to_prove with
   | [] ->
-      failwith "Empty zkapp command"
-  | witness :: rest ->
-      let%bind p1 =
-        transaction_snark_of_segment ?proving_timeout t ~sequencer_pk ~witness
-      in
-      Deferred.List.fold ~init:p1 rest ~f:(fun prev witness ->
-          let%bind curr =
-            transaction_snark_of_segment ?proving_timeout t ~sequencer_pk
-              ~witness
-          in
-          let%bind merged =
-            transaction_snark_of_merge ?proving_timeout t ~left:curr ~right:prev
-          in
-          return merged )
+      return (None, source, excess)
+  | elems_to_prove ->
+      let%bind proof, target = prover t (source, elems_to_prove) in
+      return (proof, target, excess)
 
-let transaction_snark_of_signed_command ?proving_timeout t ~sequencer_pk
-    ~(witness :
-       Mina_ledger.Sparse_ledger.t
-       * Signed_command.t
-       * Transaction_snark.Statement.With_sok.t
-       * Prover.Account_set_witness.t ) =
-  let sparse_ledger, command, stmt, account_set_witness = witness in
-  transaction_snark_of_single_signed_command ?proving_timeout t
-    ~source_ledger:stmt.source.first_pass_ledger ~sequencer_pk ~command
-    ~sparse_ledger ~account_set_witness
-
-let inner_sync ?proving_timeout t ~public_key ~ase =
-  send ?proving_timeout t (Prover.Input.Inner_sync (public_key, ase))
+let inner_sync ?proving_timeout t ~public_key ~ase_source ~ase_elms =
+  let%bind ase =
+    let%map proof, target, excess =
+      ase t ~source:ase_source ~elems:ase_elms
+        ~max_excess:Inner_sync.Ase_inst.get_iterations ase_with_length
+    in
+    Inner_sync.Ase_inst.
+      { proof; proof_target = target; init = ase_source; excess }
+  in
+  send ?proving_timeout t (Prover.Input.Inner_sync { public_key; ase })
   >>| function
   | Prover.Output.Call_forest_tree tree ->
       tree
   | _ ->
       failwith "Unexpected response from prover"
 
+let verify_both_ases ?proving_timeout t input =
+  send ?proving_timeout t (Prover.Input.Verify_both_ases input)
+  >>| function
+  | Prover.Output.Verify_both_ases snark ->
+      snark
+  | _ ->
+      failwith "Unexpected response from prover"
+
 let outer_commit ?proving_timeout t ~txn_snark ~public_key ~new_actions
     ~unprocessed_actions ~old_inner_ledger ~new_inner_ledger ~da_signature
     ~da_key =
+  let get_inner_acc ledger =
+    let inner_acc =
+      Sparse_ledger.get_exn ledger Zeko_constants.inner_account_index
+    in
+    let inner_acc_path =
+      Sparse_ledger.path_exn ledger Zeko_constants.inner_account_index
+      |> List.map ~f:(function
+           | `Left _ ->
+               ( { right_side = Field.zero }
+                 : Outer_rules.Rule_commit_inst.PathElt.t )
+           | `Right _ ->
+               { right_side = Field.one } )
+    in
+    (inner_acc, inner_acc_path)
+  in
+  let old_inner_acc, old_inner_acc_path = get_inner_acc old_inner_ledger in
+  let new_inner_acc, new_inner_acc_path = get_inner_acc new_inner_ledger in
+
+  let%bind inner_ase =
+    let ({ outer_action_state } : Rollup_state.Inner_state.t) =
+      Rollup_state.Inner_state.value_of_app_state
+        (Option.value_exn old_inner_acc.zkapp).app_state
+    in
+    let action_state : Ase.With_length.Stmt.t =
+      Rollup_state.Outer_action_state.With_length.
+        { action_state = raw outer_action_state
+        ; length = length outer_action_state
+        }
+    in
+    let%map proof, target, excess =
+      ase t ~source:action_state ~elems:new_actions
+        ~max_excess:Outer_commit.Ase_inner_inst.get_iterations ase_with_length
+    in
+    Outer_commit.Ase_inner_inst.
+      { proof; proof_target = target; init = action_state; excess }
+  in
+  let%bind outer_ase =
+    let ({ outer_action_state } : Rollup_state.Inner_state.t) =
+      Rollup_state.Inner_state.value_of_app_state
+        (Option.value_exn new_inner_acc.zkapp).app_state
+    in
+    let action_state =
+      Rollup_state.Outer_action_state.With_length.raw outer_action_state
+    in
+    let%map proof, target, excess =
+      ase t ~source:action_state ~elems:unprocessed_actions
+        ~max_excess:Outer_commit.Ase_outer_inst.get_iterations
+        ase_without_length
+    in
+    Outer_commit.Ase_outer_inst.
+      { proof; proof_target = target; init = action_state; excess }
+  in
+  let%bind verify_both_ases = verify_both_ases t (outer_ase, inner_ase) in
+
   send ?proving_timeout t
     (Prover.Input.Outer_commit
-       ( txn_snark
-       , public_key
-       , new_actions
-       , unprocessed_actions
-       , old_inner_ledger
-       , new_inner_ledger
-       , da_signature
-       , da_key ) )
+       { txn_snark
+       ; public_key
+       ; verify_both_ases
+       ; old_inner_acc
+       ; old_inner_acc_path
+       ; new_inner_acc
+       ; new_inner_acc_path
+       ; da_signature
+       ; da_key
+       } )
   >>| function
   | Prover.Output.Call_forest_tree tree ->
       tree
