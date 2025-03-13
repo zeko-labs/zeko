4f0d6d57a54c6d8dda6b9b40e3b015bfbfec8581
Martin Ondejka <ondejka99@gmail.com>
2025-03-13 16:27:07 +0000

    Use fake circuits for sequencer tests

---
+++ src/app/zeko/sequencer/prover/cli_fake.ml
@@ -0,0 +1,15 @@
+open Async
+open Signature_lib
+
+let run_server =
+  ( "run-server"
+  , Command.async ~summary:"Run prover server"
+      (let%map_open.Command port =
+         flag "--port" (required int) ~doc:"int Port to listen on"
+       in
+       let logger = Logger.create () in
+       [%log info] "Compiling circuits" ;
+       fun () -> Zeko_prover.Prover.run ~logger ~port ) )
+
+let () =
+  Command.group ~summary:"Zeko prover CLI" [ run_server ] |> Command_unix.run
