ea5b2516c9d052734231a2326188484d33682b06
Las <me@las.rs>
2025-05-12 17:18:35 +0100

    bridge: add missing file for inner receive rule (#293)

---
+++ src/app/zeko/circuits/rule_bridge_inner_receive.ml
@@ -0,0 +1,49 @@
+open Core_kernel
+open Mina_base
+open Snark_params.Tick
+module PC = Signature_lib.Public_key.Compressed
+open Bridge_state
+open Zeko_util
+
+module Make (Inputs : sig
+  val token_owner_l2 : Account_id.t option
+
+  val chain_l2 : Mina_signature_kind.t
+end) =
+struct
+  open Inputs
+
+  let token_id_l2 = token_owner_id token_owner_l2
+
+  module May_use_token = struct
+    include Account_update.May_use_token
+
+    type var = Checked.t
+  end
+
+  module Witness = struct
+    type t = { public_key : PC.t; vk_hash : F.t; amount : Currency.Amount.t }
+    [@@deriving snarky]
+  end
+
+  (** Prove that we have submitted a deposit, and that it's been accepted. *)
+  let main (w : Witness.t V.t) =
+    with_label ("main " ^ __LOC__) (fun () ->
+        let* Witness.{ public_key; vk_hash; amount } =
+          exists Witness.typ ~compute:(V.get w)
+        in
+        let account_update =
+          { default_account_update with
+            public_key
+          ; token_id = constant Token_id.typ token_id_l2
+          ; may_use_token = constant May_use_token.typ Parents_own_token
+          ; authorization_kind = authorization_vk_hash vk_hash
+          ; balance_change = Currency.Amount.Signed.Checked.(of_unsigned amount)
+          }
+        in
+        let*| out = make_outputs ~chain:chain_l2 account_update [] in
+        Compile_simple.{ prevs = No_prevs; out } )
+
+  let rule : _ Compile_simple.branch =
+    { branch_name = "inner receive"; tags = No_tags; main }
+end
