339edc847b809341c02670d13d91e58f1b58e1c8
Las <me@las.rs>
2025-06-07 02:15:42 +0000

    v: revert partially and make get and create bypass Checked toolery

--- src/app/zeko/v/v.ml
+++ src/app/zeko/v/v.ml
@@ -19,9 +19,18 @@ let typ : ('a t, 'a) Typ.t =
               t )
     }
 
-let get var = As_prover.read typ var
+let get var _ =
+  match var with
+  | Circuit_mode ->
+      failwith "Shouldn't be possible! MkRef.get run with Circuit_mode."
+  | Proving_mode t ->
+      t
 
-let create (compute : 'a As_prover.t) : 'a t Checked.t = exists typ ~compute
+let create (x : 'a As_prover.t) : 'a t Checked.t =
+  let r = ref None in
+  let c = As_prover.map x ~f:(fun x -> r := Some x) |> as_prover in
+  Checked.map c ~f:(fun () ->
+      match !r with None -> Circuit_mode | Some x -> Proving_mode x )
 
 let unsafe_unwrap : 'a t -> 'a option = function
   | Proving_mode proof ->
