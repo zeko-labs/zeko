173c097955c11b187ff73036434891025a9fb575
Geometer1729 <16kuhnb@gmail.com>
2025-06-17 16:39:53 -0400

    increase app state size

--- src/lib/mina_wire_types/mina_base/mina_base_zkapp_state.ml
+++ src/lib/mina_wire_types/mina_base/mina_base_zkapp_state.ml
@@ -2,4 +2,8 @@ module V = struct
   module V1 = struct
     type 'a t = ('a, Pickles_types.Nat.eight) Pickles_types.Vector.t
   end
+
+  module V2 = struct
+    type 'a t = ('a, Pickles_types.Nat.thirty_two) Pickles_types.Vector.t
+  end
 end
