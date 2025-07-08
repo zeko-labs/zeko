9828b00e7f4d9b66a466dd28b9f89bd3291e7779
Martin Ondejka <ondejka99@gmail.com>
2025-07-08 17:12:15 +0200

    Add max valid while to zeko constants

--- src/app/zeko/zeko_types/zeko_types.ml
+++ src/app/zeko/zeko_types/zeko_types.ml
@@ -50,7 +50,8 @@ module Inner_rules_inst =
 module Outer_rules_inst =
   Outer_rules.Make
     (struct
-      let max_valid_while_size = Slot.to_int Slot_range.infinite.upper
+      let max_valid_while_size =
+        Slot.to_int Zeko_constants.commit_max_valid_while
 
       let inner_public_key = Zeko_constants.inner_public_key
 
