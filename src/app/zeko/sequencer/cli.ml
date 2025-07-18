14c071b1eca425fa1065f2a7080f3933ea7816b4
Martin Ondejka <ondejka99@gmail.com>
2025-07-18 13:55:40 +0200

    Add prover load test to the sequencer cli (#332)

--- src/app/zeko/sequencer/cli.ml
+++ src/app/zeko/sequencer/cli.ml
@@ -1,3 +1,4 @@
+open Core_kernel
 open Async
 open Sequencer_lib
 open Signature_lib
@@ -69,7 +70,121 @@ let dump_ledger =
          Stdio.Out_channel.output_string out "]" ;
          Stdio.Out_channel.close out ) )
 
+let prover_load =
+  ( "prover-load"
+  , Command.async ~summary:"Dump the ledger"
+      (let%map_open.Command prover =
+         flag "--prover" (required string) ~doc:"string Prover server"
+       and timeout =
+         flag "--timeout" (optional float) ~doc:"float Timeout in seconds"
+       and count =
+         flag "--count"
+           (optional_with_default 1 int)
+           ~doc:"int Count of transactions"
+       in
+       fun () ->
+         let logger = Logger.create () in
+         let client =
+           Zeko_prover.Client.create ~logger
+             [ Tcp.Where_to_connect.of_host_and_port
+               @@ Core.Host_and_port.of_string prover
+             ]
+         in
+         let open Zeko_types in
+         let witness : Base_input.serializable =
+           let open Test_spec in
+           let Test_spec.{ init_ledger; specs } =
+             Quickcheck.random_value @@ Test_spec.mk_gen ~num_transactions:5 ()
+           in
+           let ledger =
+             Ledger.with_ledger
+               ~depth:Zeko_constants.constraint_constants.ledger_depth
+               ~f:(fun ledger ->
+                 List.iter (Array.to_list init_ledger)
+                   ~f:(fun (keypair, balance) ->
+                     let pk =
+                       Signature_lib.Public_key.compress keypair.public_key
+                     in
+                     let account_id = Account_id.create pk Token_id.default in
+                     let balance = Unsigned.UInt64.of_int64 balance in
+                     let account =
+                       Account.create account_id
+                         (Currency.Balance.of_uint64 balance)
+                     in
+                     Ledger.create_new_account_exn ledger account_id account ) ;
+                 Ledger.commit ledger ;
+                 ledger )
+           in
+           let imt =
+             let db =
+               Indexed_merkle_tree.Db.create
+                 ~depth:Zeko_constants.constraint_constants.ledger_depth ()
+             in
+             let tids =
+               List.map (Array.to_list init_ledger) ~f:(fun (keypair, _) ->
+                   let pk =
+                     Signature_lib.Public_key.compress keypair.public_key
+                   in
+                   let owner = Account_id.create pk Token_id.default in
+                   Account_id.derive_token_id ~owner )
+             in
+             List.iter tids ~f:(fun tid ->
+                 let _, _ =
+                   Indexed_merkle_tree.Db.get_or_create_entry_exn db tid
+                 in
+                 () ) ;
+             db
+           in
+           let command =
+             command_send ~chain:Mina_signature_kind.Testnet (List.hd_exn specs)
+           in
+           let source_acc_set =
+             Account_set.of_fields [| Indexed_merkle_tree.Db.merkle_root imt |]
+           in
+           let update_acc_set_witness =
+             let fee_payer = Signed_command.fee_payer command in
+             let receiver = Signed_command.receiver command in
+             let tids =
+               List.map [ fee_payer; receiver; fee_payer ] ~f:(fun owner ->
+                   Account_id.derive_token_id ~owner )
+             in
+             List.fold tids ~init:Acc_set_witness.empty ~f:(fun acc tid ->
+                 let _, witness =
+                   Indexed_merkle_tree.Db.get_or_create_entry_exn imt tid
+                 in
+                 Acc_set_witness.add acc witness )
+           in
+           { source_ledger = Ledger.merkle_root ledger
+           ; source_acc_set
+           ; sequencer =
+               Even_PC.create_exn
+               @@ Public_key.compress
+                    (Even_PC.generate_even_signer ()).public_key
+           ; transaction = Command command
+           ; witness =
+               { ledger_path_handler =
+                   (let aids = Signed_command.accounts_referenced command in
+                    let sparse_ledger =
+                      Sparse_ledger.of_ledger_subset_exn ledger aids
+                    in
+                    sparse_ledger )
+               ; update_acc_set_witness
+               }
+           }
+         in
+         let witnesses = List.init count ~f:(fun _ -> witness) in
+         let%map () =
+           Deferred.List.iteri ~how:`Sequential witnesses ~f:(fun i witness ->
+               printf "Proving %d/%d\n" i count ;
+               let%map _result =
+                 Zeko_prover.Client.transaction_snark ?proving_timeout:timeout
+                   client (Signed_command witness)
+               in
+               () )
+         in
+         print_endline "Done ✅" ) )
+
 let () =
   Command.group ~summary:"Sequencer CLI"
-    [ generate_even_key; migrate; dump_ledger ]
+    [ generate_even_key; migrate; dump_ledger; prover_load ]
   |> Command_unix.run
