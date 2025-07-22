48fd36723d29c954f7940114b1c8faf5633b1a82
Geometer1729 <16kuhnb@gmail.com>
2025-07-22 17:31:02 -0400

    correct more module version numbers

--- src/lib/transaction_logic/transaction_applied.ml
+++ src/lib/transaction_logic/transaction_applied.ml
@@ -167,7 +167,7 @@ module Varying : sig
   module Stable : sig
     [@@@no_toplevel_latest_type]
 
-    module V2 : sig
+    module V3 : sig
       type t [@@deriving sexp, to_yojson]
     end
   end]
@@ -186,7 +186,7 @@ end = struct
   module Stable = struct
     [@@@no_toplevel_latest_type]
 
-    module V2 = struct
+    module V3 = struct
       type t =
         | Command of Command_applied.Stable.V3.t
         | Fee_transfer of Fee_transfer_applied.Stable.V2.t
@@ -223,9 +223,9 @@ end
 module Stable = struct
   [@@@no_toplevel_latest_type]
 
-  module V2 = struct
+  module V3 = struct
     type t =
-      { previous_hash : Ledger_hash.Stable.V1.t; varying : Varying.Stable.V2.t }
+      { previous_hash : Ledger_hash.Stable.V1.t; varying : Varying.Stable.V3.t }
     [@@deriving sexp, to_yojson]
 
     let to_latest = Fn.id
