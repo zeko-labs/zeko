d58da32a93ae0dbfa72e79ce6d276a351bf20fb1
Martin Ondejka <ondejka99@gmail.com>
2025-10-16 15:07:47 +0200

    Fix var_to_events

--- src/app/zeko/circuits/zeko_util.mli
+++ src/app/zeko/circuits/zeko_util.mli
@@ -40,7 +40,7 @@ val var_to_actions :
   ('var, 'value) Typ.t -> 'var -> Mina_base.Zkapp_account.Actions.var Checked.t
 
 val var_to_events :
-  ('var, 'value) Typ.t -> 'var -> Mina_base.Zkapp_account.Actions.var Checked.t
+  ('var, 'value) Typ.t -> 'var -> Mina_base.Zkapp_account.Events.var Checked.t
 
 val var_to_hash :
   init:string -> ('var, 'value) Typ.t -> 'var -> Field.Var.t Checked.t
