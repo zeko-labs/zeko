40d66a9cf5f200c8c41ca7ac1684bd287cc556f3
Martin Ondejka <ondejka99@gmail.com>
2026-03-31 12:14:51 +0200

    Make sure sequencer starts without catchup

--- src/app/zeko/da_layer/lib/client.ml
+++ src/app/zeko/da_layer/lib/client.ml
@@ -627,7 +627,7 @@ let catch_up t ~(node_location : Host_and_port.t Cli_lib.Flag.Types.with_name)
     !"Found last ledger hash: %{sexp: Ledger_hash.t option} for node %s"
     last_ledger_hash
     (Host_and_port.to_string node_location.value) ;
-  let%map () =
+  let%bind () =
     match last_ledger_hash with
     | None ->
         return ()
@@ -661,15 +661,14 @@ let catch_up t ~(node_location : Host_and_port.t Cli_lib.Flag.Types.with_name)
           t.db_pool
         >>| caqti_ok_exn ~msg:"Failed to refetch signature: %s"
   in
-  don't_wait_for
-    (within' ~monitor:Monitor.main (fun () ->
-         start_posting_diffs_from t
-           ~timeout_on_failure:(Time_ns.Span.of_sec 10.) ~node_location
-           ~source_ledger_hash:last_ledger_hash () ) )
+  start_posting_diffs_from t ~timeout_on_failure:(Time_ns.Span.of_sec 10.)
+    ~node_location ~source_ledger_hash:last_ledger_hash ()
 
 let start_client t ~target_ledger_hash =
-  Deferred.List.iter ~how:`Parallel t.config.nodes ~f:(fun node_location ->
-      catch_up t ~node_location ~target_ledger_hash )
+  List.iter t.config.nodes ~f:(fun node_location ->
+      don't_wait_for
+        (within' ~monitor:Monitor.main (fun () ->
+             catch_up t ~node_location ~target_ledger_hash ) ) )
 
 let rec get_multisig ?pushed_signature t ~ledger_hash =
   let pushed_signature =
