5e12d4596f1c667b2be183687ebfcb6567d743c7
Hebilicious <xsh4k3@gmail.com>
2026-04-17 08:20:52 +0700

    Use Yojson 2 sequence parser

--- src/lib/cli_lib/commands.ml
+++ src/lib/cli_lib/commands.ml
@@ -1,8 +1,4 @@
 open Signature_lib
-
-(* Alias ocamlp-streams before it's hidden by open
-   Core_kernel *)
-module Streams = Stream
 open Core_kernel
 open Async
 
@@ -149,14 +145,14 @@ let validate_transaction =
     ( Command.Param.return
     @@ fun () ->
     let num_fails = ref 0 in
-    (* TODO upgrade to yojson 2.0.0 when possible to use seq_from_channel
-     * instead of the deprecated stream interface *)
-    let jsons = Yojson.Safe.stream_from_channel In_channel.stdin in
+    let num_transactions = ref 0 in
+    let jsons = Yojson.Safe.seq_from_channel In_channel.stdin in
     let signature_kind = Mina_signature_kind.t_DEPRECATED in
     ( match
         Or_error.try_with (fun () ->
-            Streams.iter
+            Stdlib.Seq.iter
               (fun transaction_json ->
+                incr num_transactions ;
                 match
                   Rosetta_lib.Transaction.to_mina_signed transaction_json
                 with
@@ -186,18 +182,15 @@ let validate_transaction =
           (Yojson.Safe.pretty_to_string (Error_json.error_to_yojson err)) ;
         Format.printf "Invalid transaction.@." ;
         Core_kernel.exit 1 ) ;
-    if !num_fails > 0 then (
+    if !num_transactions = 0 then (
+      Format.printf "Could not parse any transactions@." ;
+      exit 1 )
+    else if !num_fails > 0 then (
       Format.printf "Some transactions failed to verify@." ;
       exit 1 )
-    else
-      let first = Streams.peek jsons in
-      match first with
-      | None ->
-          Format.printf "Could not parse any transactions@." ;
-          exit 1
-      | _ ->
-          Format.printf "All transactions were valid@." ;
-          exit 0 )
+    else (
+      Format.printf "All transactions were valid@." ;
+      exit 0 ) )
 
 module Vrf = struct
   let generate_witness =
