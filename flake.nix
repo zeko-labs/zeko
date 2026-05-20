0a46dfc2063b610d6beebef2317e7b2a3c818eb2
Simonas Narbutas <simonas+icarus@narbuto.lt>
2026-05-20 13:55:27 +0300

    add zeko-signer-image output

--- flake.nix
+++ flake.nix
@@ -330,8 +330,9 @@
             libp2p_helper kimchi_bindings_stubs snarky_js validation trace-tool
             zkapp-cli;
           inherit (dockerImages)
-            zeko-image zeko-da-image zeko-archive-relay-image zeko-archive-image mina-image-slim
-            mina-image-full mina-archive-image-full mina-image-instr-full;
+            zeko-image zeko-da-image zeko-signer-image zeko-archive-relay-image
+            zeko-archive-image mina-image-slim mina-image-full
+            mina-archive-image-full mina-image-instr-full;
           mina-deb = debianPackages.mina;
           impure-shell = (import ./nix/impure-shell.nix pkgs).inputDerivation;
         }) // {
